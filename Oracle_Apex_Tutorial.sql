b329e499f902980d7b4d0fde01ce46d7483dc08d
thai vo <thaivo88@gmail.com>
2017-04-28 16:02:28 -0500

    Update Oracle_Apex_Tutorial.sql

--- Oracle_Apex_Tutorial.sql
+++ Oracle_Apex_Tutorial.sql
@@ -14,21 +14,26 @@
   COUNT(column)   return the number of rows of a column without a NULL value
   MAX(column )    return the highest value of a column
   MIN(column)     return the lowest value of a column
-  SUM(column)     return the total sum of a column*/
+  SUM(column)     return the total sum of a column
+  
+  Single Line Function (SLF)
+  UPPER(column)   return string value all in upper case
+  LOWER(column)   return string value all in lower case */
 
 /* Two main tables used in our tutorial using Oracle Apex SQL*/
 SELECT * FROM emp;
 SELECT * FROM dept;
 
 /* Selecting employees with no commission with salary greater than 1100 and less than 5000 but excluding salary with 1500 */
-SELECT * FROM emp WHERE comm IS NULL OR comm = 0
-AND sal > 1100 AND sal < 5000 
-AND sal != 1500
+SELECT * FROM emp 
+  WHERE comm IS NULL OR comm = 0
+  AND sal > 1100 AND sal < 5000 
+  AND sal != 1500
 ORDER BY comm;
 
 /* Selecting employees with the job title SALESMAN with commission of 300 or greater than 1000 */
 SELECT * FROM emp 
-WHERE job = 'SALESMAN' 
+  WHERE job = 'SALESMAN' 
 AND comm = 300 OR comm > 1000;
 
 /* AS statement is use for alias, in other words change the header name of the column */
@@ -43,3 +48,8 @@ WHERE job ='MANAGER'
   SELECT ename || ' makes $' || sal || ' per month' AS "Income" FROM emp
 WHERE job ='MANAGER'
 
+/* concatenating employee and their job title into a sentence per row */
+SELECT concat( 
+  concat(upper(ename), lower(' IS THE NAME ')), 
+  concat(lower('and their job is '), job)) 
+AS position FROM emp;
